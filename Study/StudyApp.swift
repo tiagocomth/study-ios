7500bb17cc1b564ca51979eef635d9978a52efc9
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-06-16 00:08:03 -0300

    Initial Commit

---
+++ Study/StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyApp.swift
+//  Study
+//
+//  Created by Thiago de Jesus on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
