319f8985f0c3fe320c9093e4d161dc096fb8dbc0
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-06-17 08:07:46 -0300

    feat(network): add API client

---
+++ Study/Core/Network/APIClient.swift
@@ -0,0 +1,109 @@
+//
+//  APIClient.swift
+//  Study
+//
+//  Created by Thiago de Jesus on 08/03/26.
+//
+
+import Foundation
+
+protocol APIClientProtocol {
+
+    init(session: URLSession)
+
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T
+    func request<T: Decodable>(_ path: String) async throws -> T
+}
+
+final class APIClient: APIClientProtocol {
+    private let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    func request<T>(_ endpoint: any Endpoint) async throws -> T where T : Decodable {
+
+        guard let request = RequestBuilder.build(endpoint) else {
+            //TODO: Criar um teste quando não for possivel criar o request
+            throw NetworkError.requestBuildFailed(message: "Failed to create request from endpoint.")
+        }
+
+        return try await perform(request)
+
+    }
+
+    func request<T>(_ path: String) async throws -> T where T : Decodable {
+
+        //TODO: Escrever um teste para quando a URL for inválida
+        guard let url = URL(string: path) else {
+            throw NetworkError.invalidURL(message: "Invalid URL.")
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.get.rawValue
+
+        return try await perform(request)
+
+
+    }
+
+}
+
+
+extension APIClient {
+
+
+    private func perform<T: Decodable>(_ request: URLRequest) async throws -> T {
+
+        let data: Data
+        let response: URLResponse
+        do{
+            (data, response) = try await session.data(for: request)
+        } catch(let error) {
+            //TODO: Criar um teste para quando a requisicão falhar
+            throw NetworkError.network(error)
+        }
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            //TODO: Criar um teste para quando não vir resposta
+            throw NetworkError.noResponse(message: "Invalid response from server.")
+        }
+
+        try checkStatus(status: httpResponse.statusCode)
+
+        do{
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingFailed(message: "Failed to decode data to model.")
+            //TODO: Escrever um teste para quando o decode do objeto falhar
+        }
+
+    }
+
+    //TODO: Escrever um teste para quando a requisição falhar
+    func checkStatus(status: Int) throws {
+        switch status {
+        case 200...299:
+            return
+        case 401:
+            throw NetworkError.unauthorized(
+                message: "Unauthorized request. Authentication required."
+            )
+        case 403:
+            throw NetworkError.forbidden(
+                message: "Forbidden request. You don't have permission to access this resource."
+            )
+        case 404:
+            throw NetworkError.notFound(
+                message: "Requested resource was not found."
+            )
+        default:
+            throw NetworkError.invalidStatusCode(
+                codeStatus: status,
+                message: "Unexpected status code: \(status)"
+            )
+        }
+    }
+}
