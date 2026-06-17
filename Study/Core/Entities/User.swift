0f7ab8b8f111cf9310aa3a525735ad4c391a3e3c
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-06-17 08:50:28 -0300

    feat(user): add codable identifiable User model

---
+++ Study/Core/Entities/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Study
+//
+
+import Foundation
+
+struct User: Codable, Identifiable, Equatable {
+    let id: String
+    let name: String
+    let photo: String?
+    let individualHoursTotal: Double
+    let groupHoursTotal: Double
+}
