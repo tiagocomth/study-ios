bc7381da57e55059947dd91888d6c4fa8b34e005
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-06-18 17:15:15 -0300

    refactor(network): organize files into Client/Endpoint/Auth/Models/Logging

---
+++ StudyTests/StudyTests.swift
@@ -0,0 +1,18 @@
+//
+//  StudyTests.swift
+//  StudyTests
+//
+//  Created by Thiago de Jesus on 18/06/26.
+//
+
+import Testing
+
+struct StudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
