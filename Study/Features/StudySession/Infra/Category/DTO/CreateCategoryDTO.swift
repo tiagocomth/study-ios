8d07d86ea6c6055d6d524ecdcf54bc9e6d2f9d27
Caio <caiomand04@gmail.com>
2026-06-23 11:41:09 -0300

    feat(study-session): implement category dto

---
+++ Study/Features/StudySession/Infra/Category/DTO/CreateCategoryDTO.swift
@@ -0,0 +1,10 @@
+//
+//  CreateCategoryDTO.swift
+//  Study
+//
+
+import Foundation
+
+nonisolated struct CreateCategoryDTO: Encodable, Equatable, Sendable {
+    let name: String
+}
---
+++ Study/Features/StudySession/Infra/Category/DTO/UpdateCategoryDTO.swift
@@ -0,0 +1,10 @@
+//
+//  UpdateCategoryDTO.swift
+//  Study
+//
+
+import Foundation
+
+nonisolated struct UpdateCategoryDTO: Encodable, Equatable, Sendable {
+    let name: String
+}
