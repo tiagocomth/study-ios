d461e21a9ff1bf01af0e6951e74fe00be8369c62
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-06-23 11:33:25 -0300

    feat(app): show main or auth flow based on session state

--- Study/Core/AppWorker/AppCoordinator.swift
+++ Study/Core/AppWorker/AppCoordinator.swift
@@ -8,13 +8,13 @@ import SwiftUI
 final class AppCoordinator {
     
     private var authCoordinator: AuthCoordinator?
-    
-    func makeAuthCoordinator(apiClient: APIClientProtocol) -> AuthCoordinator {
+    func makeAuthCoordinator(apiClient: APIClientProtocol, session: UserSessionProtocol) -> AuthCoordinator {
         guard let authCoordinator else {
-            self.authCoordinator = AuthCoordinator(factory: .init(apiClient: apiClient))
-            return self.authCoordinator!
+            let coordinator = AuthCoordinator(factory: .init(apiClient: apiClient, session: session))
+            self.authCoordinator = coordinator
+            return coordinator
         }
-        
+
         return authCoordinator
     }
 }
--- Study/Core/AppWorker/AppWorker.swift
+++ Study/Core/AppWorker/AppWorker.swift
@@ -43,7 +43,7 @@ final class AppWorker {
     }
 
     func makeAuthCoordinator() -> AuthCoordinator {
-        return appCoordinator.makeAuthCoordinator(apiClient: apiClient)
+        return appCoordinator.makeAuthCoordinator(apiClient: apiClient, session: userSessionService)
     }
 
 }
